77042c0d49c4dfd570fe9bbae8871562f1e629b3
github-action-benchmark <github@users.noreply.github.com>
2023-07-18 21:19:45 +0000

    add Soci Benchmark (go) benchmark result for 5491bacb496e74448c19b8aac6ee4ce30b74b2cd

--- dev/bench/perfTest/data.js
+++ dev/bench/perfTest/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689714927726,
+  "lastUpdate": 1689715185213,
   "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -44,6 +44,50 @@ window.BENCHMARK_DATA = {
             "value": 8.731,
             "unit": "pullDuration",
             "extra": "1 times\n16 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "coderbirju",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "name": "coderbirju",
+            "username": "coderbirju"
+          },
+          "id": "5491bacb496e74448c19b8aac6ee4ce30b74b2cd",
+          "message": "Add basic workflow",
+          "timestamp": "2023-06-05T22:53:45Z",
+          "url": "https://github.com/coderbirju/soci-snapshotter/pull/18/commits/5491bacb496e74448c19b8aac6ee4ce30b74b2cd"
+        },
+        "date": 1689715184075,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkThing - ns/op",
+            "value": 1557034557,
+            "unit": "ns/op",
+            "extra": "1 times\n17 procs"
+          },
+          {
+            "name": "BenchmarkThing - lazyTaskDuration",
+            "value": 0.05500000000000001,
+            "unit": "lazyTaskDuration",
+            "extra": "1 times\n17 procs"
+          },
+          {
+            "name": "BenchmarkThing - localTaskStats",
+            "value": 0.006,
+            "unit": "localTaskStats",
+            "extra": "1 times\n17 procs"
+          },
+          {
+            "name": "BenchmarkThing - pullDuration",
+            "value": 7.731,
+            "unit": "pullDuration",
+            "extra": "1 times\n17 procs"
           }
         ]
       }
