cadd1ecfe4cabc4b5d45b3a21784b7bfa5c170ce
github-action-benchmark <github@users.noreply.github.com>
2023-07-19 19:18:17 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for ba78e412dd4a9833c4f2e400f5d1f3a74e74a0f7

---
+++ dev/bench/perfTest/data.js
@@ -0,0 +1,52 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1689794297280,
+  "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
+  "entries": {
+    "Soci Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "coderbirju",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "name": "coderbirju",
+            "username": "coderbirju"
+          },
+          "id": "ba78e412dd4a9833c4f2e400f5d1f3a74e74a0f7",
+          "message": "Add basic workflow",
+          "timestamp": "2023-06-05T22:53:45Z",
+          "url": "https://github.com/coderbirju/soci-snapshotter/pull/18/commits/ba78e412dd4a9833c4f2e400f5d1f3a74e74a0f7"
+        },
+        "date": 1689794296802,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-ffmpeg-lazyTaskDuration",
+            "value": 0.8444054305,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-localTaskStats",
+            "value": 2.1839740195,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-pullDuration",
+            "value": 10.575843491499995,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-ffmpeg-fullRun",
+            "value": 11.3150453825,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      }
+    ]
+  }
+}
