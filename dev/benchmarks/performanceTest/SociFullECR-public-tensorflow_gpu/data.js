20a910f990984b29c827d74cdd7dca0e81d1ea93
github-action-benchmark <github@users.noreply.github.com>
2023-08-04 23:53:11 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for cfc206a35f6b3a863a86be023736563815552c66

---
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -0,0 +1,176 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1691193191694,
+  "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
+  "entries": {
+    "Soci Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "yogidas.a@northeastern.edu",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cfc206a35f6b3a863a86be023736563815552c66",
+          "message": "Merge pull request #48 from coderbirju/regression_automation_visualization_new\n\nMerge pull request #46 from coderbirju/regression_automation_visualiz…",
+          "timestamp": "2023-08-04T16:48:29-07:00",
+          "tree_id": "a0c2a14bca6042aa5d24531c676b8717873d6bf7",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/cfc206a35f6b3a863a86be023736563815552c66"
+        },
+        "date": 1691193187151,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 2.404,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.035,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 77.694,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yogidas.a@northeastern.edu",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cfc206a35f6b3a863a86be023736563815552c66",
+          "message": "Merge pull request #48 from coderbirju/regression_automation_visualization_new\n\nMerge pull request #46 from coderbirju/regression_automation_visualiz…",
+          "timestamp": "2023-08-04T16:48:29-07:00",
+          "tree_id": "a0c2a14bca6042aa5d24531c676b8717873d6bf7",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/cfc206a35f6b3a863a86be023736563815552c66"
+        },
+        "date": 1691193187151,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 2.404,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.035,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 77.694,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yogidas.a@northeastern.edu",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cfc206a35f6b3a863a86be023736563815552c66",
+          "message": "Merge pull request #48 from coderbirju/regression_automation_visualization_new\n\nMerge pull request #46 from coderbirju/regression_automation_visualiz…",
+          "timestamp": "2023-08-04T16:48:29-07:00",
+          "tree_id": "a0c2a14bca6042aa5d24531c676b8717873d6bf7",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/cfc206a35f6b3a863a86be023736563815552c66"
+        },
+        "date": 1691193187151,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 2.404,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.035,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 77.694,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yogidas.a@northeastern.edu",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cfc206a35f6b3a863a86be023736563815552c66",
+          "message": "Merge pull request #48 from coderbirju/regression_automation_visualization_new\n\nMerge pull request #46 from coderbirju/regression_automation_visualiz…",
+          "timestamp": "2023-08-04T16:48:29-07:00",
+          "tree_id": "a0c2a14bca6042aa5d24531c676b8717873d6bf7",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/cfc206a35f6b3a863a86be023736563815552c66"
+        },
+        "date": 1691193187151,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 2.404,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.035,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 77.694,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      }
+    ]
+  }
+}
