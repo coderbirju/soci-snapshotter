70680be8b699351d447bb473fa26440945e564ea
github-action-benchmark <github@users.noreply.github.com>
2023-08-04 16:24:40 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 2bb63b412308b5d68db85b7ce1ed65df4b7cf905

---
+++ dev/benchmarks/perfTest/home/runner/work/soci-snapshotter/soci-snapshotter/current/SociFullECR-public-tensorflow.json/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1691166280957,
+  "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
+  "entries": {
+    "Soci Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "yogidas.a@northeastern.edu",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2bb63b412308b5d68db85b7ce1ed65df4b7cf905",
+          "message": "Merge pull request #47 from coderbirju/regression_automation_visualization_new\n\nMerge pull request #46 from coderbirju/regression_automation_visualiz…",
+          "timestamp": "2023-08-04T09:18:30-07:00",
+          "tree_id": "08710435133c8e0f21b25803dac64ada70696caf",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/2bb63b412308b5d68db85b7ce1ed65df4b7cf905"
+        },
+        "date": 1691166279980,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 2.292,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.303,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 23.216,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      }
+    ]
+  }
+}
