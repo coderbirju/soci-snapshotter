413d71dae387c078d8fb9e1a2100ce2763cf2179
github-action-benchmark <github@users.noreply.github.com>
2023-08-05 19:18:22 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 91d9544875bcc3a9af1a0aee94a9b3a4ca618430

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691193192955,
+  "lastUpdate": 1691263102068,
   "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -166,6 +166,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 10.604,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yogidas.a@northeastern.edu",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "91d9544875bcc3a9af1a0aee94a9b3a4ca618430",
+          "message": "Merge pull request #49 from coderbirju/regression_automation_visualization_new\n\nMerge pull request #46 from coderbirju/regression_automation_visualiz…",
+          "timestamp": "2023-08-05T12:09:39-07:00",
+          "tree_id": "011d16aafaa140ce462625738f4c17da32c8500a",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/91d9544875bcc3a9af1a0aee94a9b3a4ca618430"
+        },
+        "date": 1691263101336,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 0.646,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.658,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 32.211,
             "unit": "Seconds",
             "extra": "P90"
           }
