12f0d183fab2801f1c1682df7064155544c41305
github-action-benchmark <github@users.noreply.github.com>
2023-08-05 19:18:24 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 91d9544875bcc3a9af1a0aee94a9b3a4ca618430

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691193184935,
+  "lastUpdate": 1691263104269,
   "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -40,6 +40,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 23.039,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yogidas.a@northeastern.edu",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "91d9544875bcc3a9af1a0aee94a9b3a4ca618430",
+          "message": "Merge pull request #49 from coderbirju/regression_automation_visualization_new\n\nMerge pull request #46 from coderbirju/regression_automation_visualiz…",
+          "timestamp": "2023-08-05T12:09:39-07:00",
+          "tree_id": "011d16aafaa140ce462625738f4c17da32c8500a",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/91d9544875bcc3a9af1a0aee94a9b3a4ca618430"
+        },
+        "date": 1691263101918,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 1.814,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 1.837,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 39.969,
             "unit": "Seconds",
             "extra": "P90"
           }
