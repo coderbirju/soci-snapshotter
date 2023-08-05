5f9102f5bafd187c354428819ba2ec613c717a61
github-action-benchmark <github@users.noreply.github.com>
2023-08-05 19:18:19 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 91d9544875bcc3a9af1a0aee94a9b3a4ca618430

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691193191694,
+  "lastUpdate": 1691263099384,
   "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -166,6 +166,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 77.694,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yogidas.a@northeastern.edu",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "91d9544875bcc3a9af1a0aee94a9b3a4ca618430",
+          "message": "Merge pull request #49 from coderbirju/regression_automation_visualization_new\n\nMerge pull request #46 from coderbirju/regression_automation_visualiz…",
+          "timestamp": "2023-08-05T12:09:39-07:00",
+          "tree_id": "011d16aafaa140ce462625738f4c17da32c8500a",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/91d9544875bcc3a9af1a0aee94a9b3a4ca618430"
+        },
+        "date": 1691263098949,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 2.65,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.014,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 226.891,
             "unit": "Seconds",
             "extra": "P90"
           }
