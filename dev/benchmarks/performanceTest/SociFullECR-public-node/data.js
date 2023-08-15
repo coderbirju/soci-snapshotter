9816fb4b65597faac2b3e74951d023044405051d
github-action-benchmark <github@users.noreply.github.com>
2023-08-15 22:27:37 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 80178b5622316be5cdb3aa95d11f1a1d538b74dc

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,9 +1,50 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692038788584,
-  "repoUrl": "https://github.com/awslabs/soci-snapshotter",
+  "lastUpdate": 1692138457922,
+  "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
-      
+      {
+        "commit": {
+          "author": {
+            "email": "yogidas.a@northeastern.edu",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "80178b5622316be5cdb3aa95d11f1a1d538b74dc",
+          "message": "Merge pull request #64 from coderbirju/fix_visualization\n\nAdd timeout of 10 secs",
+          "timestamp": "2023-08-15T15:14:16-07:00",
+          "tree_id": "314cb2f8bca8ab4023b59b5797e499af9c279361",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/80178b5622316be5cdb3aa95d11f1a1d538b74dc"
+        },
+        "date": 1692138457438,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 10.491,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 10.493500000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 11.7375,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      }
     ]
   }
 }
