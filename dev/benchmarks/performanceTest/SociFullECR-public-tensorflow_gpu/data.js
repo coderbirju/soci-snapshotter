ade97fe2b486bdb2fe468ebdcd6d7aa52ed0c225
github-action-benchmark <github@users.noreply.github.com>
2023-08-15 22:27:43 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 80178b5622316be5cdb3aa95d11f1a1d538b74dc

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,8 +1,50 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692038789795,
-  "repoUrl": "https://github.com/awslabs/soci-snapshotter",
+  "lastUpdate": 1692138463144,
+  "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "yogidas.a@northeastern.edu",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "80178b5622316be5cdb3aa95d11f1a1d538b74dc",
+          "message": "Merge pull request #64 from coderbirju/fix_visualization\n\nAdd timeout of 10 secs",
+          "timestamp": "2023-08-15T15:14:16-07:00",
+          "tree_id": "314cb2f8bca8ab4023b59b5797e499af9c279361",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/80178b5622316be5cdb3aa95d11f1a1d538b74dc"
+        },
+        "date": 1692138460585,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 13.009,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 12.342500000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 85.52099999999999,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      }
     ]
   }
 }
