b2609db48c40efd1ce0e38f2d0d133063fd0d49b
github-action-benchmark <github@users.noreply.github.com>
2023-08-23 17:19:06 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 5a40aff504535a863e0655de76a77b058184cafc

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692810802040,
+  "lastUpdate": 1692811146733,
   "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -414,6 +414,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 11.0425,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manujgrover71@gmail.com",
+            "name": "Manuj Grover",
+            "username": "manujgrover71"
+          },
+          "committer": {
+            "email": "66654647+turan18@users.noreply.github.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "distinct": true,
+          "id": "5a40aff504535a863e0655de76a77b058184cafc",
+          "message": "Using standard error package instead of go-multierror for multierror error.\n\nSigned-off-by: Manuj Grover <manujgrover71@gmail.com>",
+          "timestamp": "2023-08-22T15:49:08-04:00",
+          "tree_id": "b3c836a33b9dbca10ef7d1afb88e4e343a05e9b3",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/5a40aff504535a863e0655de76a77b058184cafc"
+        },
+        "date": 1692811144446,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 0.516,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.41300000000000003,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 9.932,
             "unit": "Seconds",
             "extra": "P90"
           }
