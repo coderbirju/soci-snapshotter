e507dd51cb4ec6815d099ed1ee11708eac300a2b
github-action-benchmark <github@users.noreply.github.com>
2023-08-23 17:19:01 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 5a40aff504535a863e0655de76a77b058184cafc

--- dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-redis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692810797490,
+  "lastUpdate": 1692811141129,
   "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -414,6 +414,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-redis-pullTaskDuration",
             "value": 3.7215000000000003,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manujgrover71@gmail.com",
+            "name": "Manuj Grover",
+            "username": "manujgrover71"
+          },
+          "committer": {
+            "email": "66654647+turan18@users.noreply.github.com",
+            "name": "Yasin Turan",
+            "username": "turan18"
+          },
+          "distinct": true,
+          "id": "5a40aff504535a863e0655de76a77b058184cafc",
+          "message": "Using standard error package instead of go-multierror for multierror error.\n\nSigned-off-by: Manuj Grover <manujgrover71@gmail.com>",
+          "timestamp": "2023-08-22T15:49:08-04:00",
+          "tree_id": "b3c836a33b9dbca10ef7d1afb88e4e343a05e9b3",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/5a40aff504535a863e0655de76a77b058184cafc"
+        },
+        "date": 1692811140639,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-redis-lazyTaskDuration",
+            "value": 0.022,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-localTaskDuration",
+            "value": 0.0235,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-redis-pullTaskDuration",
+            "value": 2.1515,
             "unit": "Seconds",
             "extra": "P90"
           }
