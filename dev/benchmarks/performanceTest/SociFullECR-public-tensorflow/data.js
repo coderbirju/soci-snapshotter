127f4032a397825f17ab1637969e6497a36b28cf
github-action-benchmark <github@users.noreply.github.com>
2024-03-07 19:48:11 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 1d4d288d008d5f9b67f7179e25e2b6f22e8d1365

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1708982951844,
+  "lastUpdate": 1709840891816,
   "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -540,6 +540,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow-pullTaskDuration",
             "value": 1.8024999999999998,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "1d4d288d008d5f9b67f7179e25e2b6f22e8d1365",
+          "message": "Update zot image tag to v2.0.1\n\nThe v2.0.0-rc6 tag got removed, so using the latest stable version\nas of the time of this commit.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-03-07T11:02:45-08:00",
+          "tree_id": "f0d00c483ca3a8d05c9f116856f02f010cdd3ebf",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/1d4d288d008d5f9b67f7179e25e2b6f22e8d1365"
+        },
+        "date": 1709840888942,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow-lazyTaskDuration",
+            "value": 23.22,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-localTaskDuration",
+            "value": 2.4025,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow-pullTaskDuration",
+            "value": 2.286,
             "unit": "Seconds",
             "extra": "P90"
           }
