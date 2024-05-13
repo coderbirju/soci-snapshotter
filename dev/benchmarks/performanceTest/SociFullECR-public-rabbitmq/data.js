97c4760c05d5d17b544f381d37b1a7cbbae0140a
github-action-benchmark <github@users.noreply.github.com>
2024-05-13 05:02:11 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 071a9111ec94afedd38f4deac7c960b803710059

--- dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-rabbitmq/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1714407136710,
+  "lastUpdate": 1715576531889,
   "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -918,6 +918,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
             "value": 1.2765,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "steven.davidovitz@dominodatalab.com",
+            "name": "Steven Davidovitz",
+            "username": "steved"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "071a9111ec94afedd38f4deac7c960b803710059",
+          "message": "Trigger Docker auth on ECR token expiry\n\nAn expired token passed to ECR will return a 403, which lacks a\nWww-Authenticate header required to trigger the docker authorizer. This\nmeant that credential helpers like amazon-ecr-credential-helper would\nnot refresh the token. This change adds the proper header to fix this\nbehavior.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-05-11T04:42:39-07:00",
+          "tree_id": "5084428f0af5e11612e6aaacf4d58e3bb790124b",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/071a9111ec94afedd38f4deac7c960b803710059"
+        },
+        "date": 1715576530882,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-rabbitmq-lazyTaskDuration",
+            "value": 12.5085,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-localTaskDuration",
+            "value": 7.6240000000000006,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-rabbitmq-pullTaskDuration",
+            "value": 1.5655000000000001,
             "unit": "Seconds",
             "extra": "P90"
           }
