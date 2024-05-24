d8d2d1284b49a002f3b1d7bbc7d3726d8a90589e
github-action-benchmark <github@users.noreply.github.com>
2024-05-24 21:39:13 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 7ead0be27fd87d56c80aedd554eb10123f6f38bc

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715711909565,
+  "lastUpdate": 1716586753156,
   "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -1128,6 +1128,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 2.495,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun Raja Yogidas",
+            "username": "coderbirju"
+          },
+          "committer": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "distinct": true,
+          "id": "7ead0be27fd87d56c80aedd554eb10123f6f38bc",
+          "message": "update getting started guide to v0.6.1\n\nSigned-off-by: Arjun Raja Yogidas <arjunry@amazon.com>",
+          "timestamp": "2024-05-24T13:49:25-07:00",
+          "tree_id": "f05456491b3c1c8fa91d91d6f9bc0117381090b9",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/7ead0be27fd87d56c80aedd554eb10123f6f38bc"
+        },
+        "date": 1716586749854,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 37.5695,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.3045,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 3.059,
             "unit": "Seconds",
             "extra": "P90"
           }
