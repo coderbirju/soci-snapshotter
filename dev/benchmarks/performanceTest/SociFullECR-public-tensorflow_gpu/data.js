f3077245dd8e28cdcd35442466f819c6c4f69590
github-action-benchmark <github@users.noreply.github.com>
2024-07-12 00:10:42 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 702560159c04ec049c5e120d02f641ae5f9586bc

--- dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-tensorflow_gpu/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718923507529,
+  "lastUpdate": 1720743042164,
   "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -1254,6 +1254,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
             "value": 2.012,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "702560159c04ec049c5e120d02f641ae5f9586bc",
+          "message": "Test against containerd 2.0 in CI\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2024-07-09T19:40:08-07:00",
+          "tree_id": "da394ac0e4950aa9b7dc1de3efdf85a7278ff26c",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/702560159c04ec049c5e120d02f641ae5f9586bc"
+        },
+        "date": 1720743040757,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-lazyTaskDuration",
+            "value": 19.8565,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-localTaskDuration",
+            "value": 2.607,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-tensorflow_gpu-pullTaskDuration",
+            "value": 3.312,
             "unit": "Seconds",
             "extra": "P90"
           }
