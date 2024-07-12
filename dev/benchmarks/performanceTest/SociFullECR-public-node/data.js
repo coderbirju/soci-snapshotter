310d291f0563f142474a31f85acecb87df8201de
github-action-benchmark <github@users.noreply.github.com>
2024-07-12 00:10:36 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 702560159c04ec049c5e120d02f641ae5f9586bc

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718923510517,
+  "lastUpdate": 1720743036521,
   "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -1170,6 +1170,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 1.8575,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "walster@amazon.com",
+            "name": "Kern Walster",
+            "username": "Kern--"
+          },
+          "committer": {
+            "email": "55906459+austinvazquez@users.noreply.github.com",
+            "name": "Austin Vazquez",
+            "username": "austinvazquez"
+          },
+          "distinct": true,
+          "id": "702560159c04ec049c5e120d02f641ae5f9586bc",
+          "message": "Test against containerd 2.0 in CI\n\nSigned-off-by: Kern Walster <walster@amazon.com>",
+          "timestamp": "2024-07-09T19:40:08-07:00",
+          "tree_id": "da394ac0e4950aa9b7dc1de3efdf85a7278ff26c",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/702560159c04ec049c5e120d02f641ae5f9586bc"
+        },
+        "date": 1720743035784,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 5.5205,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.5035000000000001,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 2.028,
             "unit": "Seconds",
             "extra": "P90"
           }
