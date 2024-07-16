6ff31ce42a63206d554d62bd217edeb1622c706d
github-action-benchmark <github@users.noreply.github.com>
2024-07-16 16:43:14 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for 9eef2c0ff55f7e52d9b2f47364dce56eaef63e9e

--- dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-mongo/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720743039762,
+  "lastUpdate": 1721148194276,
   "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -1212,6 +1212,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-mongo-pullTaskDuration",
             "value": 1.492,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davbson@amazon.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "committer": {
+            "email": "arjunry@amazon.com",
+            "name": "Arjun",
+            "username": "coderbirju"
+          },
+          "distinct": false,
+          "id": "9eef2c0ff55f7e52d9b2f47364dce56eaef63e9e",
+          "message": "Use rabbitmqImage in FUSE failure test\n\nOn ARM instances, pinnedRabbitmqImage does not work for this test due to\nsome issues with xattr/whiteout dir detection. This is either a bug in\nthe image or our code, but to get around CI for now we will just use a\nregular rabbitmq image.\n\nSigned-off-by: David Son <davbson@amazon.com>",
+          "timestamp": "2024-07-16T08:54:51-07:00",
+          "tree_id": "17760429481a83641d9808691159a22144684d68",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/9eef2c0ff55f7e52d9b2f47364dce56eaef63e9e"
+        },
+        "date": 1721148192288,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-mongo-lazyTaskDuration",
+            "value": 5.817,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-localTaskDuration",
+            "value": 0.324,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-mongo-pullTaskDuration",
+            "value": 0.9239999999999999,
             "unit": "Seconds",
             "extra": "P90"
           }
