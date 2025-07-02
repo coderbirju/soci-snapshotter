d133f9084901a9aa69db789a9b5c5b5737da8148
github-action-benchmark <github@users.noreply.github.com>
2025-07-02 17:39:39 +0000

    add Soci Benchmark (customSmallerIsBetter) benchmark result for cb2b0e41acf3884de9a89a230deb4147edef72f5

--- dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
+++ dev/benchmarks/performanceTest/SociFullECR-public-node/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747863319277,
+  "lastUpdate": 1751477979571,
   "repoUrl": "https://github.com/coderbirju/soci-snapshotter",
   "entries": {
     "Soci Benchmark": [
@@ -1338,6 +1338,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "SociFullECR-public-node-pullTaskDuration",
             "value": 2.0149999999999997,
+            "unit": "Seconds",
+            "extra": "P90"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "no-reply@github.com",
+            "name": "GitHub",
+            "username": "invalid-email-address"
+          },
+          "committer": {
+            "email": "55555210+sondavidb@users.noreply.github.com",
+            "name": "David Son",
+            "username": "sondavidb"
+          },
+          "distinct": true,
+          "id": "cb2b0e41acf3884de9a89a230deb4147edef72f5",
+          "message": "Update getting started to v0.11.1\n\nSigned-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-07-01T12:19:24-07:00",
+          "tree_id": "dc764869d8677966b6a827862836938abda142f4",
+          "url": "https://github.com/coderbirju/soci-snapshotter/commit/cb2b0e41acf3884de9a89a230deb4147edef72f5"
+        },
+        "date": 1751477978537,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SociFullECR-public-node-lazyTaskDuration",
+            "value": 3.806,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-localTaskDuration",
+            "value": 0.495,
+            "unit": "Seconds",
+            "extra": "P90"
+          },
+          {
+            "name": "SociFullECR-public-node-pullTaskDuration",
+            "value": 1.986,
             "unit": "Seconds",
             "extra": "P90"
           }
